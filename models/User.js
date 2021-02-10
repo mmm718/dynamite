6d3b9b8fddf95c6aaeeed24ba8b04cd7ca7a458b
m <xptmxmfkrndyd1234@gmail.com>
2021-02-10 11:28:58 +0900

    9

--- models/User.js
+++ models/User.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose')
 
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+
+
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -30,6 +34,28 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function( next ) {
+
+    var user = this
+
+    if(user.isModified('password')) {
+
+        // 비밀번호를 암호화 시킴
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    }else {
+        next()
+    }
+})
+
 
 const User = mongoose.model('User', userSchema)
 
